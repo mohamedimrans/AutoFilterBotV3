e57127f37f23caee13d423bdec79bbc0c45eeb2f
mohamedimrans <96737338+mohamedimrans@users.noreply.github.com>
2022-01-08 12:20:34 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -30,6 +30,7 @@ AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
 TUTORIAL = "https://t.me/PocketMoviesOfficial"
 TUTORIAL2 = "https://t.me/+eo_4Rvk0YQc4Njll"
 TRIAL = "https://t.me/Get_Movies_Here2/2"
+TRIAL2 = "https://t.me/PocketMoviesOfficial"
 # MongoDB information
 DATABASE_URI = environ['DATABASE_2']
 DATABASE_NAME = environ['BOT_NAME']
