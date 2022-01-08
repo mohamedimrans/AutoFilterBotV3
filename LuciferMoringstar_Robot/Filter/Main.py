c92255a2f4795b68da709abfb428ab4653363a64
mohamedimrans <96737338+mohamedimrans@users.noreply.github.com>
2022-01-08 12:33:26 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -60,6 +60,9 @@ async def filter(client, message):
                 file_id = file.file_id
                 filename = f"[{get_size(file.file_size)}] {file.file_name}"
                 btn.append(
+                    [InlineKeyboardButton('JOIN OUR CHANNEL', url=f'{TRIAL2}')]
+                    )
+                btn.append(
                     [InlineKeyboardButton(text=f"{filename}",callback_data=f"pr0fess0r_99#{file_id}")]
                     )
         else:
