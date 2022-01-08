3588526bf3af24dcd0c7bb03df6f066113e7e9b5
mohamedimrans <96737338+mohamedimrans@users.noreply.github.com>
2022-01-08 12:05:05 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -29,6 +29,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
 TUTORIAL = "https://t.me/PocketMoviesOfficial"
 TUTORIAL2 = "https://t.me/+eo_4Rvk0YQc4Njll"
+TRIAL = "https://t.me/Get_Movies_Here2/2"
 # MongoDB information
 DATABASE_URI = environ['DATABASE_2']
 DATABASE_NAME = environ['BOT_NAME']
